5feed02ab0073ecc0cef6fa9da86b6c5a53f5b04
Antonio Marangi <antonio.marangi@canonical.com>
2026-09-15 12:24:43 +0200

    fix tests

--- pkg/machine/machine_test/dummy-machine_test.go
+++ pkg/machine/machine_test/dummy-machine_test.go
@@ -14,6 +14,7 @@ func init() {
 			{
 				Architecture:   "amd64",
 				ManufacturerId: "GenuineIntel",
+				BrandString:    "12th Gen Intel(R) Core(TM) i7-12850HX",
 				Flags:          []string{"fpu", "vme", "de"},
 			},
 		},
